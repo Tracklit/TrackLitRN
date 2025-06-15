7044781eff8ab623871bc2e4a5dcf2eebab801ce
lionmartinez <41510051-lionmartinez@users.noreply.replit.com>
2025-06-15 19:06:52 +0000

    Restored to '577a3c1aad2c1dd4a8cf12f7799d81dc882dd9bc'
    Replit-Restored-To: 577a3c1aad2c1dd4a8cf12f7799d81dc882dd9bc

--- client/src/pages/home-page.tsx
+++ client/src/pages/home-page.tsx
@@ -230,7 +230,7 @@ export default function HomePage() {
       href: "/practice",
       disabled: false,
       isSpecial: true,
-      backgroundImage: practiceBackground,
+      backgroundImage: programsBackground,
       hasBackground: true
     },
     {
@@ -248,7 +248,7 @@ export default function HomePage() {
       icon: <Trophy className="h-6 w-6 text-primary" />,
       href: "/meets",
       disabled: false,
-      backgroundImage: practiceBackground,
+      backgroundImage: raceBackground,
       hasBackground: true
     },
     {
@@ -257,7 +257,7 @@ export default function HomePage() {
       icon: <Clock className="h-6 w-6 text-primary" />,
       href: "/training-tools",
       disabled: false,
-      backgroundImage: practiceBackground,
+      backgroundImage: toolsBackground,
       hasBackground: true
     },
     {
