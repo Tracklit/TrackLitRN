996939ba3ff136294bd8050dbbc679ef6b876d58
lionmz123 <lionmartinez@gmail.com>
2025-09-12 08:53:27 +0000

    fix Capacitor config - ready for iOS build

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.tracklit.app',
+  appName: 'TrackLit',
+  webDir: 'dist/public'
+};
+
+export default config;
